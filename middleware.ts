f04073d8528757dc2ad9d04e8daf557b5cc63b1c
George Moon <george@moon.org.nz>
2023-07-02 16:22:38 +0200

    Added redirection to middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,19 @@ import type { Database } from "@/types/supabase";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
-  await supabase.auth.getSession();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // if user is not signed in and the current path is not /login redirect the user to /login
+  if (!user && req.nextUrl.pathname !== "/login") {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
   return res;
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
