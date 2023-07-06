1c36d92526f128b6d8d688895c3f6fd8b3d367f2
George Moon <george@moon.org.nz>
2023-07-07 00:43:27 +0200

    Simplifying middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,16 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-import type { NextRequest } from "next/server";
 import type { Database } from "@/types/supabase";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient<Database>({ req, res });
 
-  const { data, error } = await supabase.auth.getSession();
-  if (error) console.log(error);
-
-  const user = data.session?.user;
+  const { data } = await supabase.auth.getSession();
 
   // Anonymous user redirect (except for login page itself)
-  if (!user && req.nextUrl.pathname !== "/account") {
+  if (!data.session?.user && req.nextUrl.pathname !== "/account") {
     const url = new URL("/account", req.url);
     url.searchParams.append("next", req.nextUrl.pathname); // Store original URL as a search parameter called 'next'
 
