ec5bac4b9be269a547e27f6fa961bdb5db32b7c3
George Moon <george@moon.org.nz>
2023-07-10 22:05:15 +0200

    Added Event link back to Records

--- app/records/RecordsList.tsx
+++ app/records/RecordsList.tsx
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
-import { DateTime } from "luxon";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import type { Database } from "@/types/supabase";
@@ -9,7 +8,9 @@ import Table from "@/components/ui/Table";
 export default async function RecordsList() {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  const { data: records } = await supabase.from("records").select("*");
+  const { data: records } = await supabase
+    .from("records")
+    .select("*, event");
 
   if (!records) return <em>No records found</em>;
 
@@ -19,6 +20,8 @@ export default async function RecordsList() {
         <Table.Row>
           <Table.Heading>ID</Table.Heading>
           <Table.Heading>Protocol</Table.Heading>
+          <Table.Heading>Type</Table.Heading>
+          <Table.Heading>Event</Table.Heading>
         </Table.Row>
       </Table.Head>
       <Table.Body>
@@ -28,6 +31,12 @@ export default async function RecordsList() {
               <Link href={`/records/${record.id}`}>{record.id}</Link>
             </Table.Data>
             <Table.Data>{record.protocol}</Table.Data>
+            <Table.Data>{record.type}</Table.Data>
+            <Table.Data>
+              <Link href={`/events/${record.event}`}>
+                Event: {record.event}
+              </Link>
+            </Table.Data>
           </Table.Row>
         ))}
       </Table.Body>
