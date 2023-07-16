1243c04a7164f2279e3cc1872f15b91650478057
George Moon <george@moon.org.nz>
2023-07-17 01:14:51 +0200

    Added commit hash to footer to help with debugging

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,28 +1,29 @@
 import Link from "next/link";
 import { Session } from "@supabase/auth-helpers-nextjs";
 
-import Message from "@/components/ui/Message";
-const { name, version } = require("@/package.json");
+const { name } = require("@/package.json");
 
 export default async function Footer({ session }: { session: Session | null }) {
   return (
-    <footer className="bg-light py-3 text-center">
+    <footer className="bg-light py-3 mt-5 text-center fs-6">
       <div className="container-fluid">
-        <p className="mb-1">
-          {process.env.NEXT_PUBLIC_DATABASE_NAME}{" "}
+        <p>
+          <strong>{process.env.NEXT_PUBLIC_DATABASE_NAME}</strong>
+        </p>
+        <p className="mb-0">
+          {session?.user && (
+            <small>
+              Logged in as <Link href="/account">{session.user.email}</Link>
+            </small>
+          )}
+        </p>
+        <p className="mb-0">
           <small>
-            ({name}@{version})
+            Powered by {name}
+            {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA &&
+              ` (${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA})`}
           </small>
         </p>
-        <small>
-          {session?.user ? (
-            <p>
-              Logged in as <Link href="/account">{session.user.email}</Link>
-            </p>
-          ) : (
-            <Message>Not logged in</Message>
-          )}
-        </small>
       </div>
     </footer>
   );
