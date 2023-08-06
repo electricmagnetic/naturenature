2d5cb35f8ce84818c245c58cd6a26c7d1dcefeb8
George Moon <george@moon.org.nz>
2023-08-06 14:32:01 +0200

    Tweaks to Icon and dropdowns

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -3,12 +3,16 @@ import clsx from "clsx";
 export default function Icon({
   iconName,
   className,
+  label,
   ...others
 }: {
   iconName: string;
   className?: string;
+  label?: string;
 }) {
-  return (
-    <i className={clsx(`bi`, `bi-${iconName}`, className)} {...others}></i>
-  );
+  const classes = clsx(`bi`, `bi-${iconName}`, !label && "me-2", className);
+
+  if (label) return <i className={classes} aria-label={label} {...others}></i>;
+
+  return <i className={classes} aria-hidden="true" {...others}></i>;
 }
