cb0e269dedf5a088a7e1046d0d7ece90c5fd6141
George Moon <george@moon.org.nz>
2023-08-29 20:43:48 +0200

    Adding lookup functions to individuals and objects (identifiers/samples)

--- app/(entities)/individuals/api/client.ts
+++ app/(entities)/individuals/api/client.ts
@@ -2,6 +2,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import revalidate from "@/components/helpers/revalidate";
 import type { Database } from "@/types/_supabase";
+import type { LookupItem } from "@/types/database";
 import type { IndividualDto } from "../types";
 
 export const upsertIndividual = async (values: IndividualDto) => {
@@ -16,4 +17,27 @@ export const upsertIndividual = async (values: IndividualDto) => {
   if (status === 201) await revalidate("/individuals");
 
   return { status, data, error };
+};
+
+export const lookupIndividuals = async (search?: string) => {
+  const supabase = createClientComponentClient<Database>();
+
+  const query = search
+    ? supabase
+        .from("individuals")
+        .select("id, name")
+        .textSearch("name", `'${search}':*`)
+        .returns<LookupItem[]>()
+    : supabase
+        .from("individuals")
+        .select("id, name")
+        .order("created_at")
+        .limit(10)
+        .returns<LookupItem[]>();
+
+  const { data, error } = await query;
+  if (error) throw Error(error.message);
+  if (!data) return [];
+
+  return data;
 };
