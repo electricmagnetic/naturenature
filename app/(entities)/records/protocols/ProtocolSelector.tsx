6fd5a21032441839505ba78033ece5aa77585c7c
George Moon <george@moon.org.nz>
2023-09-02 14:38:06 +0200

    ProtocolSelector now uses a query string

--- app/(entities)/records/protocols/ProtocolSelector.tsx
+++ app/(entities)/records/protocols/ProtocolSelector.tsx
@@ -1,15 +1,31 @@
+import { useCallback } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
 import Lookup from "@/components/dictionary/Lookup";
 import NavLink from "@/components/layout/NavLink";
 import { protocolMetadata } from "./metadata";
 
 export default function ProtocolSelector() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams],
+  );
+
   return (
     <>
       <ul className="nav flex-column nav-special">
         {Object.entries(protocolMetadata).map(([protocol, metadatum]) => (
           <NavLink
             key={protocol}
-            href={`/records/new/?protocol=${protocol}`}
+            href={`${pathname}?${createQueryString("protocol", protocol)}`}
             iconName={metadatum.iconName}
           >
             <strong>{metadatum.name}</strong>
